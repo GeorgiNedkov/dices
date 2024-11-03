9fedec5dfe6562ded6ab71d418dc79a5d93a64c6
georgi nedkov <georgiqwertyuiop@mail.bg>
2024-11-03 23:45:15 +0200

    clear

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 ARCH=arm64
-docker build --platform linux/amd64 --pull --rm -f "dockerfile" -t dice-server-x86:latest "."
-docker build --pull --rm -f "dockerfile" -t dice-server-m1:latest "."
+docker build --platform linux/amd64 --pull --rm -f "Dockerfile" -t dice-server-x86:latest "."
+docker build --pull --rm -f "Dockerfile" -t dice-server-m1:latest "."
