889246b957433b6b4da83d186160e6e6d5120277
georgi nedkov <georgiqwertyuiop@mail.bg>
2024-11-06 21:11:23 +0200

    update build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,8 @@
+IMAGE_NAME=dice-server-x86:latest
+
+docker build --platform linux/amd64 --pull --rm -f "Dockerfile" -t $IMAGE_NAME "."
+
 ARCH=arm64
-docker build --platform linux/amd64 --pull --rm -f "Dockerfile" -t dice-server-x86:latest "."
+IMAGE_NAME=dice-server-m1:latest
+
 docker build --pull --rm -f "Dockerfile" -t dice-server-m1:latest "."
